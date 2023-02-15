e119b47ea28a8a60f8a1f28edc74a3d8402f4f0f
maikb <maik.basel@gmx.de>
2023-02-15 21:16:44 +0100

    chore: Setup project

---
+++ src/main/java/com/maikbasel/hexjavamoduledemo/HexJavaModuleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.maikbasel.hexjavamoduledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexJavaModuleDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexJavaModuleDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maikbasel/hexjavamoduledemo/HexJavaModuleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maikbasel.hexjavamoduledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexJavaModuleDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
